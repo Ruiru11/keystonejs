7de98e9b29bb633207f5cfccdf0ecbd00b065082
Erastus Ruiru <erastusruiru@Erastuss-MacBook-Pro.local>
2019-04-04 18:37:32 +0300

    first commit

---
+++ routes/views/products.js
@@ -0,0 +1,18 @@
+const keystone = require('keystone');
+
+exports = module.exports = function (req, res) {
+	const view = new keystone.View(req, res);
+	const locals = res.locals;
+
+	locals.section = 'store';
+
+	view.query(
+		'products',
+		keystone
+			.list('Product')
+			.model
+			.find()
+	);
+
+	view.render('products');
+};
